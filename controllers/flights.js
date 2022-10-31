c53745b5a77ce3805ac4a4566a4d9f7a0630caa3
Kaleb2515 <zuhars@icloud.com>
2022-10-31 17:48:58 -0500

    refactor the redirect lines 31-35 controllers->flights.js makes nodemon crash

--- controllers/flights.js
+++ controllers/flights.js
@@ -15,4 +15,21 @@ function newFlights(req, res) {
     res.render('flights/new');
 }
 
+function create(req, res) {
 
+    // remove any whitespace at start and end of departs
+    req.body.departs = req.body.departs.trim();
+    // split cast into an array if it's not an empty string - using a regular expression as a separator
+    if (req.body.departs) req.body.departs = req.body.departs.split(/\s*,\s*/);
+    // remove empty properties
+    for (let key in req.body) {
+      if (req.body[key] === "") delete req.body[key];
+    }
+}
+
+
+//   flights.save(function (err) {
+//     if (err) return res.redirect('/flights/new');
+//       console.log(flights);
+//       res.redirect('/flights');
+//   });
